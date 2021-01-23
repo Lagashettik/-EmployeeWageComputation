b8b304ed1dcd3c6c484189c398f80a420dabf2a1
Lagashettik <Lagashettik@gmail.com>
2021-01-23 15:23:14 +0530

    Added: Employee Monthly Wage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -2,29 +2,24 @@
 public class EmpWageBuilder {
 	public static void main(String args[]){
 	System.out.println("Welcome to Employee Wage Computation");
-	
-	int isFullTime=1;
-	int isPartTime=2;
+
 	int WagePerHour=20;
 	int NoOfHours=0;
-	int empCheck=(int) Math.floor(Math.random() * 10) % 3;
+	int Month=20;
+	int empCheck=(int) Math.floor(Math.random() * 10) % 2;
 
 	switch (empCheck) {
 	case 1:
-		System.out.println("Employee is Present");
+		System.out.println("Employee is Full Time\nFull Time");
 		NoOfHours=8;
 		break;
-	case 2:
-		System.out.println("Employee is Part Time");
+	case 0:
+		System.out.println("Employee is Part Time\nPart Time");
 		NoOfHours=4;
 		break;
-	case 0:
-		System.out.println("Employee is absent");
-		NoOfHours=0;
-		break;
-		}
-	int Payment= WagePerHour * NoOfHours;
-	System.out.println("Payment is "+Payment);
+	}
+	int Payment= WagePerHour * NoOfHours * Month;
+	System.out.print("\bPayment for One Month "+Payment);
 	
 	}
 }
